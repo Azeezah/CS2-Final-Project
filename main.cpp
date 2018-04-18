c9092646f689a70530d5a3ddb74ec4fb8941e30c
Azeezah <azeezahm14@gmail.com>
2018-04-17 22:01:02 -0400

    add temporary song class
    The song class is a dependency of the playlist class.  Future versions must include a string member "title" to work with the current playlist class.

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include "playlist.h"
+
+using namespace std;
+
+int main(){}
---
+++ song.h
@@ -0,0 +1,13 @@
+#ifndef SONG_H
+#define SONG_H
+#include <string>
+
+using namespace std;
+
+struct Song {
+ int num_likes;
+ string title;
+ string artist;
+};
+
+#endif
