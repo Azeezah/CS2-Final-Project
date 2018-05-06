99ca9cb6d6ca29b363dab5902ac6c102e3dfb470
Travis Brown <travjbrown98@gmail.com>
2018-05-06 00:18:59 -0400

    fixed sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+#include<iomanip>
+#include "sort.h"
+using namespace std;
+
+template<typename TheType>
+void Sort::Merge(TheType playlist, int i, int j, int k) {
+    int mergedSize = k - i + 1;                // Size of merged partition
+    int mergePos = 0;                          // Position to insert merged number
+    int leftPos = 0;                           // Position of elements in left partition
+    int rightPos = 0;                          // Position of elements in right partition
+    TheType* mergedNumbers = new TheType[mergedSize];  // Dynamically allocates temporary array
+    // for merged numbers
+    
+    leftPos = i;                               // Initialize left partition position
+    rightPos = j + 1;                          // Initialize right partition position
+    
+    // Add smallest element from left or right partition to merged numbers
+    while (leftPos <= j && rightPos <= k) {
+        if (playlist[leftPos] < playlist[rightPos]) {
+            mergedNumbers[mergePos] = playlist[leftPos];
+            ++leftPos;
+        }
+        else {
+            mergedNumbers[mergePos] = playlist[rightPos];
+            ++rightPos;
+            
+        }
+        ++mergePos;
+    }
+    
+    // If left partition is not empty, add remaining elements to merged numbers
+    while (leftPos <= j) {
+        mergedNumbers[mergePos] = playlist[leftPos];
+        ++leftPos;
+        ++mergePos;
+    }
+    
+    // If right partition is not empty, add remaining elements to merged numbers
+    while (rightPos <= k) {
+        mergedNumbers[mergePos] = playlist[rightPos];
+        ++rightPos;
+        ++mergePos;
+    }
+    
+    // Copy merge number back to numbers
+    for (mergePos = 0; mergePos < mergedSize; ++mergePos) {
+        playlist[i + mergePos] = mergedNumbers[mergePos];
+    }
+}
+template<typename TheType>
+void Sort::MergeSort(TheType playlist, int i, int k) {
+    int j = 0;
+    if (i < k) {
+        j = (i + k) / 2;  // Find the midpoint in the partition
+        
+        // Recursively sort left and right partitions
+        MergeSort(playlist, i, j);
+        MergeSort(playlist, j + 1, k);
+        
+        // Merge left and right partition in sorted order
+        Merge(playlist, i, j, k);
+    }
+}
+
+
